9ef70180eff4d3eb631c52b62f815cc6ec5451ea
muqeemahmad12 <103497679+muqeemahmad12@users.noreply.github.com>
2022-05-12 11:06:13 +0530

    removed unused code

--- DocereeAdsSdk/DocereeAdViewRichMediaBanner.swift
+++ DocereeAdsSdk/DocereeAdViewRichMediaBanner.swift
@@ -644,20 +644,20 @@ extension String {
         return (self.range(of: pattern, options: .regularExpression, range: nil, locale: nil) != nil)
     }
 }
-
-extension UIApplication{
-    class func topViewController(controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
-        if let navigationController = controller as? UINavigationController {
-            return topViewController(controller: navigationController.visibleViewController)
-        }
-        if let tabController = controller as? UITabBarController {
-            if let selected = tabController.selectedViewController {
-                return topViewController(controller: selected)
-            }
-        }
-        if let presented = controller?.presentedViewController {
-            return topViewController(controller: presented)
-        }
-        return controller
-    }
-}
+//
+//extension UIApplication{
+//    class func topViewController(controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+//        if let navigationController = controller as? UINavigationController {
+//            return topViewController(controller: navigationController.visibleViewController)
+//        }
+//        if let tabController = controller as? UITabBarController {
+//            if let selected = tabController.selectedViewController {
+//                return topViewController(controller: selected)
+//            }
+//        }
+//        if let presented = controller?.presentedViewController {
+//            return topViewController(controller: presented)
+//        }
+//        return controller
+//    }
+//}
